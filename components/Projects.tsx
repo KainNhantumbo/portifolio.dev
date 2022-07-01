a42512e428b94579a602f4d90587b12bdb7343fa
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-01 10:36:40 +0200

    feat: added more projects.

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
 import Image, { StaticImageData } from 'next/image';
 import { ProjectsContainer as Container } from '../styles/components/projects';
-import {
-	HiBadgeCheck,
-	HiLink,
-	HiPaperAirplane,
-	HiPlay,
-	HiViewGrid,
-} from 'react-icons/hi';
+import { HiBadgeCheck, HiPlay, HiViewGrid } from 'react-icons/hi';
 
 // image assets
 import anime_blog from '../assets/anime-blog.jpeg';
@@ -35,11 +29,12 @@ enum Categories {
 	back = 'Back-end',
 	front = 'Front-end',
 	full = 'Full-stack',
+	train = 'Training',
 }
 
 const projects: ProjectsProps[] = [
 	{
-		name: 'Notes application Front-end and Full-Stack Projects',
+		name: 'Notes Application made with React.JS',
 		category: Categories.front,
 		image: nava_notes,
 		live_url: 'https://nava-notes-app.vercel.app',
@@ -58,7 +53,58 @@ const projects: ProjectsProps[] = [
 		category: Categories.full,
 		image: recipes_website,
 		live_url: '',
-		code_url: 'https://github.com/KainNhantumbo/Planner-App',
+		code_url: '',
+	},
+	{
+		name: 'Hagira E-commerce Website made using Javascript, React.JS and Node.JS + Express.JS + Mongo DB on the Back-end',
+		category: Categories.full,
+		image: server_01,
+		live_url: '',
+		code_url: 'https://github.com/KainNhantumbo/Hagira-Brands-Store',
+	},
+	{
+		name: 'Server Restfull API for Hagira E-commerce Website, made with Javascript, Node.JS, Express.JS and Mongo DB',
+		category: Categories.back,
+		image: server_01,
+		live_url: '',
+		code_url: 'https://github.com/KainNhantumbo/Hagira-Website-API',
+	},
+	{
+		name: 'Server Restfull API for Contacts and Tasks App, made with Javascript, Node.JS, Express.JS and Mongo DB',
+		category: Categories.back,
+		image: server_03,
+		live_url: '',
+		code_url: 'https://github.com/KainNhantumbo/Server-for-planner-app',
+	},
+	{
+		name: 'Anime Blog, Javascript + HTML5 + CSS',
+		category: Categories.front,
+		image: anime_blog,
+		live_url:
+			'https://kainnhantumbo.github.io/Static-Anime-Blog--made-with-HTML--CSS-and-Javascript',
+		code_url: '',
+	},
+	{
+		name: 'Repository with Python training projects, exercises and mini-games',
+		category: Categories.train,
+		image: python_repo,
+		live_url: '',
+		code_url: '',
+	},
+	{
+		name: 'Multi-functions Calculator, Javascript + HTML5 + SASS',
+		category: Categories.front,
+		image: the_calculator,
+		live_url: 'https://kainnhantumbo.github.io/The-Calculator',
+		code_url: '',
+	},
+	{
+		name: 'Ecommerce Landing Page, HTML5 + CSS + SASS',
+		category: Categories.train,
+		image: ecommerce_page,
+		live_url:
+			'https://kainnhantumbo.github.io/Website-Theme-Marketplace-E-Commerse-Landing-Page',
+		code_url: '',
 	},
 ];
 
@@ -67,7 +113,7 @@ export default function Projects(): JSX.Element {
 		<Container>
 			<h2>
 				<HiViewGrid />
-				<span>My Projects</span>
+				<span>My Projects and Works</span>
 			</h2>
 			<section className='cards-container'>
 				<h3 className='sub-title'>
