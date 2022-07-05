5238e6a7bb4e4a9fc3852015f1db780e5433c42b
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-05 18:56:26 +0200

    performance: optimized font fetching.

--- components/Head.tsx
+++ components/Head.tsx
@@ -6,16 +6,6 @@ export default function HeadPage() {
 			<meta name='author' content='Kain Nhantumbo' />
 			<meta name='description' content='Kain Nhantumbo portifolio website' />
 			<meta name='viewport' content='initial-scale=1.0, width=device-width' />
-			<link rel='preconnect' href='https://fonts.googleapis.com' />
-			<link
-				rel='preconnect'
-				href='https://fonts.gstatic.com'
-				crossOrigin={'true'}
-			/>
-			<link
-				href='https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600&display=swap'
-				rel='stylesheet'
-			/>
 			<title>Kain Nhantumbo Portifolio</title>
 		</Head>
 	);
