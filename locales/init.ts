946256aa5bc457ee593bf1ec4b12ec1755b8dd3e
Kain Nhantumbo <nhantumbok@gmail.com>
2023-07-21 20:18:27 +0200

    chore: removed i18next-browser-languagedetector package. Release 3.0.2.

--- locales/init.ts
+++ locales/init.ts
@@ -2,21 +2,17 @@ import i18next from 'i18next';
 import pt_BR from './languages/pt_BR.json';
 import en_US from './languages/en_US.json';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18next
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    debug: false,
-    lng: 'en',
-    partialBundledLanguages: false,
-    fallbackLng: ['en', 'pt'],
-    interpolation: { escapeValue: false },
-    resources: {
-      pt: { translation: pt_BR },
-      en: { translation: en_US },
-    },
-  });
+i18next.use(initReactI18next).init({
+  debug: false,
+  lng: 'en',
+  partialBundledLanguages: false,
+  fallbackLng: ['en', 'pt'],
+  interpolation: { escapeValue: false },
+  resources: {
+    pt: { translation: pt_BR },
+    en: { translation: en_US },
+  },
+});
 
 export default i18next;
