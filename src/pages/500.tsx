5bf9e38dcb0263ce85e76a287a7b26c80a7143bf
Kain Nhantumbo <nhantumbok@gmail.com>
2023-10-15 17:23:35 +0200

    feat: added 500 error page

---
+++ src/pages/500.tsx
@@ -0,0 +1,22 @@
+import Package from '../../package.json';
+import { useRouter } from 'next/router';
+import { errorPage as Container } from '../styles/error-page';
+
+export default function InternalServerError() {
+  const router = useRouter();
+  return (
+    <Container>
+      <section className='logo-container'>
+        <div className='logo'>
+          <span>{Package.website_name}</span>
+        </div>
+      </section>
+      <section className='content-container'>
+        <h1>500</h1>
+        <h2>Ooops! That's an error: internal server error...</h2>
+        <p>Sorry, but looks like something wrong happened on the server.</p>
+        <button onClick={() => router.reload()}>Retry</button>
+      </section>
+    </Container>
+  );
+}
