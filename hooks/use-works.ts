befe298e2eb2b2f573bd391168b90fc3d4471ea2
Kain Nhantumbo <nhantumbok@gmail.com>
2024-03-15 13:07:02 +0200

    feat: added one more project

--- hooks/use-works.ts
+++ hooks/use-works.ts
@@ -3,12 +3,29 @@
 import type { Work } from '@/types';
 import choconoteyImage from '@/../public/assets/notes-demo-light.png';
 import palletoneDemoImage from '@/../public/assets/palletone-demo.png';
+import rubymartDemoImage from '@/../public/assets/rubymart-demo.png';
+
 import { useScopedI18n } from '@/locales/client';
 
 export const useWorks = (): readonly Work[] => {
   const translation = useScopedI18n('works');
 
   return [
+    {
+      title: translation('rubymart.title'),
+      description: translation('rubymart.description').split('\n'),
+      image: rubymartDemoImage,
+      livePreview: {
+        label: translation('live_preview_label'),
+        url: 'https://sales-app-demo.vercel.app'
+      },
+      repository: {
+        label: translation('repository_label'),
+        url: 'https://github.com/KainNhantumbo/sales-app'
+      },
+      stack: ['node.js', 'next.js', 'styled-components', 'express.js', 'typescript'],
+      platforms: ['Web', 'PWA Apps']
+    },
     {
       title: translation('choconotey.title'),
       description: translation('choconotey.description').split('\n'),
@@ -21,7 +38,7 @@ export const useWorks = (): readonly Work[] => {
         label: translation('repository_label'),
         url: 'https://github.com/KainNhantumbo/notes-client'
       },
-      stack: ['node.js', 'react.js', 'express.js', 'typescript'],
+      stack: ['node.js', 'react.js', 'express.js', 'styled-components', 'typescript'],
       platforms: ['Web', 'PWA Apps']
     },
     {
