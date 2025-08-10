c37d26c67be3e4cbf7fd3c2617734c2377159569
Kain Nhantumbo <nhantumbok@gmail.com>
2025-08-10 20:13:31 +0200

    chore: updated nextjs config. Updated some packages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  swcMinify: true,
-  optimizeFonts: false,
   productionBrowserSourceMaps: false,
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true
-  }
 };
