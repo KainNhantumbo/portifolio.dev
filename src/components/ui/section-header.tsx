e68e8fdb04252205f0ed5cb1a61fc167c27f6d0a
Kain Nhantumbo <nhantumbok@gmail.com>
2025-11-15 14:32:13 +0200

    feat: created a separated component for section headers

---
+++ src/components/ui/section-header.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { m as motion } from 'framer-motion';
+
+interface Props {
+  title: string;
+  description: string;
+  className?: string;
+}
+
+export function SectionHeader(props: Props) {
+  return (
+    <div className={cn('mb-6 space-y-6 lg:mb-12', props.className)}>
+      <motion.div
+        initial='initial'
+        whileHover='hovered'
+        className='relative block overflow-hidden'>
+        <motion.div
+          variants={{
+            initial: { y: 0 },
+            hovered: { y: '-100%' }
+          }}>
+          <h2 className='font-slab text-5xl font-bold uppercase leading-relaxed sm:text-5xl lg:text-6xl xl:text-[7rem] 2xl:text-[8rem]'>
+            <span>{props.title}</span>
+          </h2>
+        </motion.div>
+        <motion.div
+          className='absolute inset-0'
+          variants={{
+            initial: { y: '100%' },
+            hovered: { y: 0 }
+          }}>
+          <h2 className='font-slab text-5xl font-bold uppercase leading-relaxed sm:text-5xl lg:text-6xl xl:text-[7rem] 2xl:text-[8rem]'>
+            <span>{props.title}</span>
+          </h2>
+        </motion.div>
+      </motion.div>
+
+      <h3 className='w-full max-w-4xl text-lg font-medium sm:text-xl lg:leading-relaxed'>
+        {props.description}
+      </h3>
+    </div>
+  );
+}
