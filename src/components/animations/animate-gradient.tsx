947a9f55baab5456bb8e95b265259035ad7b0240
Kain Nhantumbo <nhantumbok@gmail.com>
2025-11-22 18:30:15 +0200

    refactor: added support to style externaly animated gradient component

--- src/components/animations/animate-gradient.tsx
+++ src/components/animations/animate-gradient.tsx
@@ -1,7 +1,16 @@
-export function AnimateGradient() {
+import { cn } from '@/lib/utils';
+
+interface Props {
+  className?: string;
+}
+
+export function AnimateGradient({ className }: Props) {
   return (
     <div
-      className={`rounded-tr-ful h-8 w-screen animate-gradient bg-gradient-to-r from-primary via-[#E73C7E] to-[#23A6D5] bg-[length:400%_400%]`}
+      className={cn(
+        `rounded-tr-ful h-8 w-screen animate-gradient bg-gradient-to-r from-primary via-[#E73C7E] to-[#23A6D5] bg-[length:400%_400%]`,
+        className
+      )}
     />
   );
 }
