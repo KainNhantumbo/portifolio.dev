b0121dc22600a758cc38db2d8bb289e003a01cd4
Kain Nhantumbo <nhantumbok@gmail.com>
2025-12-09 19:35:38 +0200

    perf: optimized vortex effect

--- src/components/animations/animate-vortex.tsx
+++ src/components/animations/animate-vortex.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { cn } from '@/lib/utils';
-import { motion } from 'framer-motion';
+import { m as motion } from 'framer-motion';
 import * as React from 'react';
 import { createNoise3D } from 'simplex-noise';
 
@@ -38,7 +38,7 @@ export const AnimateVortex = (props: VortexProps) => {
   const xOff = 0.00125;
   const yOff = 0.00125;
   const zOff = 0.0005;
-  const backgroundColor = props.backgroundColor || '#000000';
+  const backgroundColor = props.backgroundColor || '#00000000';
   let tick = 0;
   const noise3D = createNoise3D();
   let particleProps = new Float32Array(particlePropsLength);
