cf6d510b2c8358ed44ea2049042bee14abd44d00
Kain Nhantumbo <nhantumbok@gmail.com>
2025-12-11 20:12:07 +0200

    feat: added posts list animations

--- src/app/[locale]/blog/_components/post-list.tsx
+++ src/app/[locale]/blog/_components/post-list.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { AnimatedTextFlip } from '@/components/animations/animate-text-flip';
 import type { Post } from '@/types';
 import * as React from 'react';
 import PostCard from './post-card';
@@ -9,15 +10,24 @@ interface Props {
   locale: string;
 }
 
+const titleWords: string[] = ['Notebook', 'Book', 'Posts', 'Notes', 'Annotations'];
+
 const PostList = ({ posts, locale }: Props) => {
   return (
-    <article className='relative mx-auto flex min-h-screen w-full max-w-[1280px] flex-col items-center gap-3 pt-5'>
-      <section className='grid w-full grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-3'>
-        {posts.map((post, index) => (
-          <PostCard key={index} post={post} locale={locale} />
-        ))}
-      </section>
-    </article>
+    <section className='space-y-6'>
+      <h2 className='text-center font-slab text-xl uppercase leading-relaxed md:text-4xl lg:text-5xl xl:text-6xl'>
+        Meet my
+        <AnimatedTextFlip words={titleWords} className='text-primary' />
+      </h2>
+
+      <article className='relative mx-auto flex min-h-screen w-full max-w-[1280px] flex-col items-center gap-3 pt-5'>
+        <section className='grid w-full grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-3'>
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post} locale={locale} />
+          ))}
+        </section>
+      </article>
+    </section>
   );
 };
 
