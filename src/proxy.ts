acc0daf49e55592637bd02f2ba750e63e262bf6d
Kain Nhantumbo <nhantumbok@gmail.com>
2026-04-26 12:58:29 +0200

    deps: upgraded next to version 16

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,7 +7,7 @@ const I18nMiddleware = createI18nMiddleware({
   defaultLocale: 'en'
 });
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   return I18nMiddleware(request);
 }
 
